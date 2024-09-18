abb7278338097bce1381069127cabeba6c825ae6
THEJESWAR21 <thejeswar21jeeva@gmail.com>
2024-09-18 23:24:47 +0530

    Working

--- 01-Bagels.py
+++ 01-Bagels.py
@@ -1,31 +1,36 @@
+# To-Do
+# [x] - Varaibles for generating random 3 digital values
+# [x] - A loop to only provide the user with 10 guesses
+# [x] - a statement that printed bagel whenever all the values are incorrect
+# [ ] - make the clues into a list (Pico And Fermi) 
+    # The Loop Should interate through the str one but one (For Loop)
+# [ ] - Exception Handling
+
 import random
 
-# To-Do
-# [ ] - Game Menu
-# [x] - Varaibles for generating random 3 digital values
-# [ ] - A For Loop To provide an input whenever the userinput is false
-# [ ] - Function that check user input with the answer and provide an output
-# [ ] - Error Handling
+clues = []
 
-ans = tuple(str(random.randrange(100,999)))
+# ans = str(random.randrange(100,999))
+ans = "701"
 print(ans)
-
-clues = ""
 
 for chances in range(1,11):
     print(f"Guess #{chances}")
-    guess = tuple(input(" > "))
-    if ans == guess:
+    guess = input(">>>> ")
+
+    for i in range(len(guess)):
+        if guess[i] == ans[i]:
+            # One Digit is correct and in the right position
+            clues.append("Fermi")
+        elif guess[i] in ans:
+            # One digit is correct but in the incorrent position
+            clues.append("Pico")
+
+    if guess == ans:
+        print("You Won!")
         break
-    else:
-        for ans_ind, ans_val in enumerate(ans):
-            for guess_ind, guess_val in enumerate(guess):
-                if ans_val == guess_val and ans_ind != ans_val:
-                    clues = "Pico"
-                    print(clues)
-                elif ans_val == guess_val and ans_ind == ans_val:
-                    clues = "Fermi"
-                    print(clues)
-                elif ans_val != guess_val and ans_ind != ans_val:
-                    clues = "Bagel"
-                    print(clues)
+    elif len(clues) == 0:
+        clues.append("Bagels")
+    
+    print(*clues)
+    clues.clear()
