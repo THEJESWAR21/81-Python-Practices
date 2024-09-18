63a39a0945549aa1ca84c4366faec7de3f9b6e4a
THEJESWAR21 <thejeswar21jeeva@gmail.com>
2024-09-18 20:21:59 +0530

    Bagel0.7

--- 01-Bagels.py
+++ 01-Bagels.py
@@ -1,9 +1,31 @@
 import random
 
 # To-Do
-# [x] - Game Menu
+# [ ] - Game Menu
 # [x] - Varaibles for generating random 3 digital values
-# [x] - A For Loop To provide an input whenever the userinput is false
+# [ ] - A For Loop To provide an input whenever the userinput is false
 # [ ] - Function that check user input with the answer and provide an output
 # [ ] - Error Handling
 
+ans = tuple(str(random.randrange(100,999)))
+print(ans)
+
+clues = ""
+
+for chances in range(1,11):
+    print(f"Guess #{chances}")
+    guess = tuple(input(" > "))
+    if ans == guess:
+        break
+    else:
+        for ans_ind, ans_val in enumerate(ans):
+            for guess_ind, guess_val in enumerate(guess):
+                if ans_val == guess_val and ans_ind != ans_val:
+                    clues = "Pico"
+                    print(clues)
+                elif ans_val == guess_val and ans_ind == ans_val:
+                    clues = "Fermi"
+                    print(clues)
+                elif ans_val != guess_val and ans_ind != ans_val:
+                    clues = "Bagel"
+                    print(clues)
--- test.py
+++ test.py
@@ -2,13 +2,18 @@ import random
 
 ans = str(random.randrange(100,999))
 print(tuple(ans))
-guess =  str(random.randrange(100,999))
-print(tuple(guess))
+guess =  tuple(input("> "))
 
 
 for ans_index, ans_value in enumerate(ans):
     for guess_index, guess_value in enumerate(guess):
         if ans_value == guess_value:
             print(f"Match Found: {ans_value} at index {ans_index} in tuple1 and index {guess_index} in tuple2")
+            if ans_index == guess_index:
+                print("Fermi")
+            elif ans_index != guess_index:
+                print("Pico")
+            else:
+                print("Bagel")
         else:
             print("No Match Found")
