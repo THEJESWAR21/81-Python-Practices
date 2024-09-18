6f08b011e06bb7d6559518ac369a2f03b982a4ef
THEJESWAR21 <thejeswar21jeeva@gmail.com>
2024-09-19 00:13:38 +0530

    Final Edits"

--- 01-Bagels.py
+++ 01-Bagels.py
@@ -2,35 +2,64 @@
 # [x] - Varaibles for generating random 3 digital values
 # [x] - A loop to only provide the user with 10 guesses
 # [x] - a statement that printed bagel whenever all the values are incorrect
-# [ ] - make the clues into a list (Pico And Fermi) 
+# [x] - make the clues into a list (Pico And Fermi) 
     # The Loop Should interate through the str one but one (For Loop)
-# [ ] - Exception Handling
+# [x] - Exception Handling
 
 import random
 
 clues = []
+ans = str(random.randrange(100,999))
 
-# ans = str(random.randrange(100,999))
-ans = "701"
-print(ans)
-
-for chances in range(1,11):
-    print(f"Guess #{chances}")
-    guess = input(">>>> ")
-
-    for i in range(len(guess)):
-        if guess[i] == ans[i]:
-            # One Digit is correct and in the right position
-            clues.append("Fermi")
-        elif guess[i] in ans:
-            # One digit is correct but in the incorrent position
-            clues.append("Pico")
-
-    if guess == ans:
-        print("You Won!")
-        break
-    elif len(clues) == 0:
-        clues.append("Bagels")
+def main():
+    print("""
+Bagels, a deductive logic game.
+By Al Sweigart al@inventwithpython.com
+I am thinking of a 3-digit number. Try to guess what it is.
+Here are some clues:
+When I say:    That means:
+    Pico         One digit is correct but in the wrong position.
+    Fermi        One digit is correct and in the right position.
+    Bagels       No digit is correct.
+I have thought up a number.
+You have 10 guesses to get it.
+""")
     
-    print(*clues)
-    clues.clear()
+    for chances in range(1,11):
+        print(f"Guess #{chances}")
+        guess = input("> ")
+
+        for i in range(len(guess)):
+            if guess[i] == ans[i]:
+                # One Digit is correct and in the right position
+                clues.append("Fermi")
+            elif guess[i] in ans:
+                # One digit is correct but in the incorrent position
+                clues.append("Pico")
+
+        if guess == ans:
+            print("You got it!")
+            end_game()
+            break
+        elif len(clues) == 0:
+            clues.append("Bagels")
+        
+        print(*clues)
+        clues.clear()
+
+
+def end_game():
+    print("Do you want to play again? (yes or no)")
+    end_game = input("> ")
+    if end_game == 'yes':
+        main()
+    elif end_game == 'no':
+        exit()
+
+
+if "__main__" == __name__:
+    try:
+        main()
+        print("Actual Answer: ",ans)
+    except Exception as e:
+        print(f"An error occured: {e}")
