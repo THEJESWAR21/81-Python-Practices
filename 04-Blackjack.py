db00ccf1c3445735b1c6c9970f6416ce11e5f04d
THEJESWAR21 <thejeswar21jeeva@gmail.com>
2024-09-25 22:38:50 +0530

    Value Cal #1

--- 04-Blackjack.py
+++ 04-Blackjack.py
@@ -1,8 +1,12 @@
 # Todo
-# [x] - Ask the input to get the bet value
-# [x] - A Loop to create a deck of 52 cards
-# [ ] - Return the first random 2 card hit from both player and dealer
-# [ ] - Make the hit , stand and Double down option work
+# [x] - A Function to ge the bet input
+# [x] - A Function to create all 52 deck of cards
+# [ ] - Give the dealer and player two cards from the deck each 
+# [ ] - Hand Value Calculation
+# [ ] - Game loop for the player moves
+# [ ] - Display hands and cards
+# [ ] - win, loss logic
+# [ ] - Error Handling
 
 
 import random
@@ -18,6 +22,8 @@ class Blackjack:
     def __init__(self) -> None:
         self.bet = self.get_bet()
         self.deck = self.get_deck()
+        self.PLAYER = 0
+        self.DEALER = 0
 
     def get_bet(self):
         print('How much do you bet? (1-5000, or QUIT) ')
@@ -36,9 +42,44 @@ class Blackjack:
         print(deck)
         return deck
     
+    def get_cards(self):
+        while True:
+            DEALER_Card = random.choice(self.deck) 
+            PLAYER_Card = random.choice(self.deck)
+            
+            match DEALER_Card[1]:
+                case 'J', 'Q', 'K':
+                    self.DEALER += 10
+                case 'A':
+                    if self.DEALER + 11 > 21:
+                        self.DEALER += 1
+                    else:
+                        self.DEALER += 11
+                case 2,3,4,5,6,7,8,9,10,11:
+                    self.DEALER += int(DEALER_Card[1])
+    
+            match PLAYER_Card[1]:
+                case 'J', 'Q', 'K':
+                    self.PLAYER += 10
+                    return self.PLAYER
+                case 'A':
+                    if self.PLAYER + 11 > 21:
+                        self.PLAYER += 1
+                        return self.PLAYER
+                    else:
+                        self.PLAYER += 11
+                        return self.PLAYER
+                case 2,3,4,5,6,7,8,9,10,11:
+                    self.PLAYER += int(PLAYER_Card[1])
+                    return self.PLAYER
+                    
+            break
+        
     def play_game(self):
-        DEALER = 0
-        PLAYER = 0
+        self.get_cards()
+        print(self.DEALER)
+        print(self.PLAYER)
 
         
-Blackjack()
+
+Blackjack().play_game()
