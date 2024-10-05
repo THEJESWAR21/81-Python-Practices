6d3a9882905404df39abd82f869ab670d6fdb5cf
THEJESWAR21 <thejeswar21jeeva@gmail.com>
2024-10-05 18:51:36 +0530

    Hand Value

--- 04-Blackjack.py
+++ 04-Blackjack.py
@@ -2,7 +2,7 @@ import random
 # Todo
 # [x] - A Function to get the bet input
 # [x] - A Function to create all 52 deck of cards
-# [ ] - Give the dealer and player two cards from the deck each 
+# [x] - Give the dealer and player two cards from the deck each 
 # [ ] - Hand Value Calculation
 # [ ] - Game loop for the player moves
 # [ ] - Display hands and cards
@@ -21,6 +21,8 @@ class Blackjack:
         self.DECK = self.create_deck()
         self.PlAYER_HAND = []
         self.DEALER_HAND = []   
+        self.PLAYER = 0
+        self.DEALER = 0
 
     def get_bet(self):
         print("How much do you bet? (1-5000, or QUIT)")
@@ -41,12 +43,30 @@ class Blackjack:
     def play_game(self):
         print("Bet:",self.BET_VALUE)
 
-        # Players get 2 cards 
+        # Players get 2 cards at the start 
         for x in range(2):
             self.PlAYER_HAND.append(random.choice(self.DECK))
-            self.DEALER_HAND.append(random.choice(self.DECK))
-            
-
+            if x == 0:
+                self.DEALER_HAND.append(self.BACKSIDE)
+            else:
+                self.DEALER_HAND.append(random.choice(self.DECK))
+        
+        self.hand_value()
         print(self.PlAYER_HAND)
         print(self.DEALER_HAND)
+        print(self.PLAYER)
+
+    def hand_value(self):
+        while True:
+            for x in range(len(self.PlAYER_HAND)):
+                for y in range(1):
+                    try:
+                        i = int(self.PlAYER_HAND[x][y])
+                    except ValueError:
+                        self.PLAYER += 10
+                    else:
+                        self.PLAYER += i
+                        
+            break
+
 Blackjack().play_game()
