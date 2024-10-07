1f29c8e65e8b3fba8c096b942b987fdc320576dc
THEJESWAR21 <thejeswar21jeeva@gmail.com>
2024-10-07 23:58:03 +0530

    Final Edits

--- 04-Blackjack.py
+++ 04-Blackjack.py
@@ -1,24 +1,24 @@
 import random 
+
 # Todo
 # [x] - A Function to get the bet input
 # [x] - A Function to create all 52 deck of cards
 # [x] - Give the dealer and player two cards from the deck each 
 # [x] - Hand Value Calculation
 # [x] - Game loop for the player moves
-# [ ] - win, loss logic
-# [ ] - Display hands and cards
-# [ ] - Error Handling
+# [x] - win, loss logic
+# [x] - Error Handling
 
 class Blackjack:
-    #Unicode Characters for suits
+    # Unicode Characters for suits
     HEARTS = chr(9829)
     DIAMONDS = chr(9830)
     SPADES = chr(9824)
     CLUBS = chr(9827)
     BACKSIDE = "backside"
 
-
-    def __init__(self):
+    def __init__(self) -> None:
+        """Initlizes a new game state"""
         self.BET_VALUE = self.get_bet()
         self.DECK = self.create_deck()
         self.PlAYER_HAND = []
@@ -33,7 +33,6 @@ class Blackjack:
 
 
     def create_deck(self):
-        """Create 52 card deck with tuples in a list"""
         deck = []
         for suits in (self.HEARTS, self.DIAMONDS, self.SPADES, self.CLUBS):
             for rank in range(2,11):
@@ -49,15 +48,17 @@ class Blackjack:
         self.calculate_player_hand()
         dealer_display_hand = [card if card != self.BACKSIDE else '????' for card in self.DEALER_HAND]
         print("Player's hand: ", self.PlAYER_HAND)
-        print("Dealer's hand:", dealer_display_hand)
+        print("Player's hand value:", self.PLAYER)
+        print("")
 
-        print("player's hand value:", self.PLAYER)
         if self.BACKSIDE in self.DEALER_HAND:
-            print("Dealer's Hand Value: ????")
+            print(f"Dealer's Hand: {dealer_display_hand}")
+            print("")
         else:
-            self.calculate_dealer_hand()
+            print("Dealer's Hand:", self.DEALER_HAND)
             print(f"Dealer's Hand Value: {self.DEALER}")
-
+            print("")
+        
 
     def calculate_player_hand(self):
         self.PLAYER = 0
@@ -71,10 +72,6 @@ class Blackjack:
             elif card == 'A':
                 aces += 1
                 self.PLAYER += 11
-        
-        while self.PLAYER > 21 and aces:
-            self.PLAYER -= 10
-            aces -= 1
         
 
     def calculate_dealer_hand(self):
@@ -92,9 +89,6 @@ class Blackjack:
                 aces += 1
                 self.DEALER += 11
                 
-        while self.DEALER > 21 and aces:
-            self.DEALER -= 10
-            aces -= 1
 
     def player_turn(self):
         while True:
@@ -107,11 +101,12 @@ class Blackjack:
                 if self.PLAYER >= 21:
                     print("BUST! You went over 21")
                     break
+                else:
+                    self.dealer_turn()
 
             elif move == "S":
                 self.dealer_turn()
                 break
-
             elif move == "D":
                 self.BET_VALUE = self.BET_VALUE * 2
                 print(self.BET_VALUE)
@@ -132,24 +127,29 @@ class Blackjack:
         self.DEALER_HAND[0] = random.choice(self.DECK)
         self.calculate_dealer_hand()
 
-        while self.DEALER < 17:
-            self.DEALER_HAND.append(random.choice(self.DECK))
-            self.DEALER_HAND()
-            self.display_hands()
-    
-        self.check_winner()
-    
-    def check_winner(self):
-        if self.DEALER > 21:
-            print("Dealer busts! You win!")
-        elif self.PLAYER > self.DEALER:
-            print("You win with a hand value of", self.PLAYER)
+        while True:
+            if self.DEALER < 17:
+                self.DEALER_HAND.append(random.choice(self.DECK))
+                self.calculate_dealer_hand()
+                self.display_hands()
+                self.player_turn()
+                break
+            else:
+                print("Dealer Busts! You win!")
+                self.display_hands()
+                self.compare_score()
+                break
 
-        elif self.DEALER > self.PLAYER:
-            print("Dealer wins with a hand value of", self.DEALER)
+
+    def compare_score(self):
+        print("\n Final Scores:")
+        print(f"Player: {self.PLAYER}, Dealer: {self.DEALER}")
+        if self.PLAYER > self.DEALER:
+            print("You win!")
+        elif self.PLAYER < self.DEALER:
+            print("Dealer wins!")
         else:
             print("It's a tie!")
-
 
     def play_game(self):
         print("Bet:",self.BET_VALUE)
@@ -163,7 +163,9 @@ class Blackjack:
                 self.DEALER_HAND.append(random.choice(self.DECK))
         
         self.display_hands()
-        self.player_turn()
+        if self.player_turn():
+            self.compare_score()
 
 
-Blackjack().play_game()
+if __name__ == '__main__':
+    Blackjack().play_game()
