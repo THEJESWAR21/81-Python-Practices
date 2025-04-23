1047472b6da534957ad14ffcf2ca7adf78786255
THEJESWAR21 <thejeswar21jeeva@gmail.com>
2025-04-23 16:06:24 +0530

    More Bagels

--- 01-Bagels.py
+++ 01-Bagels.py
@@ -1,24 +1,48 @@
 import random
 
-answer = random.randint(100,999)
-print(answer)
-clues =  []
+numDigits = 3
+secretNum = str(701)
+print(secretNum)
+
+print(
+'''Bagels, a deductive logic game.
+ By Al Sweigart al@inventwithpython.com
+  
+ I am thinking of a {}-digit number with no repeated digits.
+ Try to guess what it is. Here are some clues:
+ When I say:    That means:
+    Pico         One digit is correct but in the wrong position.
+    Fermi        One digit is correct and in the right position.
+    Bagels       No digit is correct.
+  
+ For example, if the secret number was 248 and your guess was 843, the
+ clues would be Fermi Pico.'''.format(numDigits))
+
 for num in range(1,11):
     print(f"Guess #{num}:")
     guess = str(input("> "))
-    if guess == clues:
-        print("You got it!")
 
-    for i in range(len(clues)):
-        if guess[i] == clues[i]:
-            clues.append('Fermi')
-        elif guess[i] in clues:
-            clues.append('Pico')
-        print(clues)
-
-        
+    try:
+        clues = []
+        for i in range(len(secretNum)):
+            if guess[i] == secretNum[i]:
+                clues.append("Fermi")
+            elif guess[i] in secretNum:
+                clues.append("Pico")
+    
+        print(*clues)
+        if guess == secretNum:
+            print("You got it!")
+            break
+        elif num == 10:
+            print(f"You Loss The Secret Number is {secretNum}")
+            print("Thanks For Planning")
+    except Exception as e:
+        print("Error Try Again!")
+        break
 
     if len(clues) == 0:
         clues.append("Bagels")
-
+    else:
+        clues.sort()
 
