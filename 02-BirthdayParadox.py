092d0e26e3e8c1e18eda7a245aab43b8fdd7d3c0
THEJESWAR21 <thejeswar21jeeva@gmail.com>
2025-06-18 19:51:53 +0530

    BirthdayParadox OOP

--- 02-BirthdayParadox.py
+++ 02-BirthdayParadox.py
@@ -3,63 +3,70 @@ import calendar
 import statistics
 
 
-def calculate_date(num):
-    dates = []
-    for _ in range(num):
-        month_index = random.randint(1,12)
-        month_days = calendar.monthrange(2023, month_index)[1]
-        month = calendar.month_abbr[month_index] # need to slice the list as [1]
-        days = random.randint(1, month_days)
-
-        date = f"{month} {days}"
-        dates.append(date)
-    return dates
-
-print('How many birthdays shall I generate? (Max 100)')
-num = int(input("> "))
-
-dates = calculate_date(num)
-print(f'Here are {num} birthdays:')
-print(*dates, sep=', ')
-
-try:
-    most_common = statistics.mode(dates)
-    print(f"In this simulation, multiple people have a birthday on {most_common}")
-except statistics.StatisticsError:
-    print("No unique most common birthday in this sample.")
-
-print("\nLet's run another 100,000 simulations.")
-print("0 simulations run...")
-
-simulations = 0
-total_matches = 0
-input("Press Any Key to Start Simulation...")
-while simulations < 100_000:
-    dates = calculate_date(num) # Calling the Function
-
-    if len(set(dates)) < len(dates):
-        total_matches += 1
-    simulations += 1
+class BirthdayParadox:
+    def __init__(self, year=2023):
+        self.year = year
+        self.dates = []
 
 
-    if simulations == 10_000:
-        print("10000 simulations run...")
-    if simulations == 50_000:
-        print("50000 simulations run...")
-    if simulations == 90_000:
-        print("90000 simulations run...")
+    def generate_birthdays(self, group_size):
+        self.dates = []
+        for _ in range(group_size):
+            month_index = random.randint(1,12)
+            month_days = calendar.monthrange(self.year, month_index)[1]
+            day = random.randint(1, month_days)
+            month = calendar.month_abbr[month_index]
+            self.dates.append(f"{month} {day}")
+        return self.dates
+    
+    def display_birthdays(self):
+        print(f'Here are {len(self.dates)} birthdays:')
+        print(",".join(self.dates))
 
+    def find_most_common_birthday(self):
+        try:
+            most_common = statistics.mode(self.dates)
+            print(f"In this simulation, multiple people have a birthday on {most_common}")
+        except statistics.StatisticsError:
+            print("No unique most common birthday in this sample.")
+    
+    def run_simulations(self, group_size, num_simulations=100_000):
+        print("\nLet's run another 100,000 simulations.")
+        print("0 simulations run...")
+        total_matches = 0
 
-probability = round(total_matches / 100_000 * 100, 2) 
+        for simulation in range(1, num_simulations + 1):
+            birthdays = self.generate_birthdays(group_size)
+            if len(set(birthdays)) < len(birthdays):
+                total_matches += 1
 
-print("100000 simulations run.")
-print(f"""
-      Out of 100,000 simulations of {num}, there was a
+            if simulation in [10_000, 50_000, 90_000]:
+                print(f'{simulation} simulations run...')
+
+        probability = round(total_matches / num_simulations * 100, 2)
+        print("100000 simulations run.")
+        print(f"""
+      Out of 100,000 simulations of {group_size} people, there was a
       matching birthday in that group {total_matches} times. This means
-      that {num} people have a {probability} % chance of having a matching
+      that {group_size} people have a {probability}% chance of having a matching
       birthday in their group. 
 
-      That's probably more than you would
-      
-      """
-      )
+      That's probably more than you would expect!
+      """)
+    
+def main():
+    print('How many birthdays shall I generate? (Max 100)')
+    group_size = int(input("> "))
+
+    simulator = BirthdayParadox()
+    simulator.generate_birthdays(group_size)
+    simulator.display_birthdays()
+    simulator.find_most_common_birthday()
+
+    input("Press Any Key to Start Simulation...")
+    simulator.run_simulations(group_size)
+
+
+if __name__ == "__main__":
+    main()
+
