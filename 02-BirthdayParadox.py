31b4ec20bb77b4144afd5edc3d2b6c4443daaaf4
THEJESWAR21 <thejeswar21jeeva@gmail.com>
2025-10-12 23:57:49 +0530

    BirthdayParadox V1.0

--- 02-BirthdayParadox.py
+++ 02-BirthdayParadox.py
@@ -1,44 +1,63 @@
 import random
 import datetime
+import statistics
 
 print("How Many Birthdays Shall I Generate? (MAX 100)")
 user_days = int(input("> "))
 
 year = 2025
-
 birthdays = []
-birthdays_set = set()
-
 
 def generate_birthdays():
+    birthdays.clear()
     start_date = datetime.date(year,1,1)
     end_date = datetime.date(year,12,31)
 
     days_between = (end_date - start_date).days
     
-    for i in range(user_days):
+    for _ in range(user_days):
         random_day = random.randint(start_date.day, days_between) # 1 - 364 is the range
         random_date = start_date + datetime.timedelta(days=random_day)
 
         birthdays.append(str(random_date.strftime("%b") + " " + str(random_date.day)))
-        birthdays_set.add(str(random_date.strftime("%b") + " " + str(random_date.day)))
 
-def same_birthday():
-    commonality = list(set(birthdays) ^ birthdays_set)
-    commonality_num = len(birthdays) - len(birthdays_set)
+def finding_duplicates():
+    # Unique_birthdays = set(birthdays)
+    duplicates = set()
+    for i in birthdays:
+        if birthdays.count(i) > 1:
+            duplicates.add(i)
+    return len(duplicates)
 
-    print(commonality)
-    print(commonality_num)
+def simualtions():
+    total_duplicate_count = 0
+    for i in range(100_001):
+        generate_birthdays()
+        count = finding_duplicates()
+        total_duplicate_count += count
 
+        if (i % 10_000 == 0 or i == 100_000):
+            print(f"{i} simulation run...")
 
+    percentage = (total_duplicate_count / 100_000) * 100
+    print(f"Out of 100,000 simulations of {user_days} people, there was a matching birthday in that group {total_duplicate_count} times. This means""")
+    print(f"{percentage}")
+    return total_duplicate_count
+        
 def main():
     generate_birthdays()
 
-    print("Here are 23 Birthdays: ")
-    same_birthday()
+    print(f"Here are {user_days} Birthdays: ")
+    print(", ".join(birthdays))
 
+    if finding_duplicates:
+        duplicates_mode = statistics.mode(birthdays)
+        print(f"\nIn this simulation, multiple people have a birthday on {duplicates_mode}")
+    else:
+        print("\nNo shared birthdays found in this simulation!") 
 
+    count = finding_duplicates()
+    
+    simualtions()
 
 main()
-
-
--- test.py
+++ test.py
@@ -1,5 +1,4 @@
 list1 = [0,1,2,3,4,6,9,99]
 list2 = [0,1,55,66,7,7,9]
 
-commonitems = list(set(list1) & set(list2))
-print(commonitems)
+print(list2.count(7))
