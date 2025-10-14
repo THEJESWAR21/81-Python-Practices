18c72a1c8eb72b4a6b2b1adac91dfd278c95fad0
THEJESWAR21 <thejeswar21jeeva@gmail.com>
2025-10-14 23:10:24 +0530

    BitmapMessage V1.1

--- 03-BitmapMessage.py
+++ 03-BitmapMessage.py
@@ -1,5 +1,3 @@
-
-
 bitmap = """
 ....................................................................
     **************   *  *** **  *      ******************************
@@ -24,17 +22,30 @@ bitmap = """
 ....................................................................
 
 """
-print("Enter the message to display with the bitmap")
-message = input("> ")
-if message == "":
-    exit()
 
-for lines in bitmap.splitlines():
-  for index, bit in enumerate(lines):
-     if bit == "*" or bit == ".":
-       print(message[index % len(message)], end="")
-     else:
-        print(" ",end='')
-  print()
-  #  print(message[index % len(message)])
+## Gets User Input
+def get_input(): 
+  print("Enter the message to display with the bitmap")
+  message = input("> ")
+  if message == "":
+      exit()
+  return message
+ 
+def main():
+  text = get_input()
+  for lines in bitmap.splitlines(): ## Separtes each Lines
+    for index, bit in enumerate(lines):
+      if bit == "*" or bit == ".":
+        print(text[index % len(text)], end="") # because 
+        # 0 % 5 = 0 | 1 % 5 = 1 | 2 % 5 = 2 | 3 % 5 = 3 | 4 % 5 = 4 | 5 % 5 = 0 | 6 % 5 = 1
+      else:
+          print(" ",end='')
+    print() # need space between each printed line
+    
+    #  print(message[index % len(message)])
 
+main()
+
+# Learn OOP
+# Learn Nested For Loops
+# Learn Print Parameter Values
--- test.py
+++ test.py
@@ -1,4 +1,13 @@
-string = "Hello ifaihfdi fihasihdif ai"
+bitmap = "jaksjkfjalsjdlfalksddf"
+string = "Hello"
 
-for i in string:
-    print(i, end="")
+# for index, bit in enumerate(bitmap):
+#     print(string[index % len(string)])
+
+print(0 % 5) # 0
+print(1 % 5) # 1
+print(2 % 5) # 2
+print(3 % 5) # 3
+print(4 % 5) # 4
+print(5 % 5) # 0
+print(6 % 5) # 1
